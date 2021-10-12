d78a300a38025ec6269b2eaf2be5df543ac3fb44
Matthew Kwong <matthewkwong2@gmail.com>
2021-10-12 22:46:54 +0800

    [Optimization] NVM,change of font stack does not reduce layout shift

--- src/brandingTheme/systemFonts.js
+++ src/brandingTheme/systemFonts.js
@@ -1,13 +1,4 @@
 export default [
   "Rubik",
-  "-apple-system",
-  "BlinkMacSystemFont",
-  "'Segoe UI'",
-  "Roboto",
-  "'Helvetica Neue'",
-  "Arial",
-  "sans-serif",
-  "'Apple Color Emoji'",
-  "'Segoe UI Emoji'",
-  "'Segoe UI Symbol'"
+  "sans-serif"
 ];
