e7e37d52256cc1898b9c00cac8c954515512ffd0
kmw111 <kmw111@ha.org.hk>
2021-10-15 13:50:50 +0800

    [Bug Fix] NotFoundPage stye and SSR

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,6 +9,9 @@ const useSx = () => useMemo(() => ({
     alignItems: "center",
     justifyContent: "center",
     height: "100vh"
+  },
+  404: {
+    color: "primary.main"
   }
 }), []);
 
@@ -18,17 +21,19 @@ const NotFoundPage = () => {
   return (
     <Container component="main">
       <SEO title="Not Found" />
-      <Stack spacing={4} sx={sx.content}>
-        <Typography variant="h1">
-          404
-        </Typography>
-        <Typography variant="h5">
-          The page you were looking for does not exist.
-        </Typography>
-        <Button variant="contained" color="secondary" size="large" component={Link} to="/">
-          go back to home
-        </Button>
-      </Stack>
+      <div>
+        <Stack spacing={4} sx={sx.content}>
+          <Typography variant="h1" sx={sx[404]}>
+            404
+          </Typography>
+          <Typography variant="h5">
+            The page you were looking for does not exist.
+          </Typography>
+          <Button variant="contained" color="secondary" size="large" component={Link} to="/">
+            go back to home
+          </Button>
+        </Stack>
+      </div>
     </Container>
   );
 };
