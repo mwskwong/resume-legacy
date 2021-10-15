c35882d4b492ac7d5dff6a7b65fc87d6dfe449af
kmw111 <kmw111@ha.org.hk>
2021-10-15 13:26:42 +0800

    [Refactor] NotFoundPage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,14 @@
-import { Button, Container, Typography } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
 
 import { Link } from "gatsby";
 import SEO from "components/SEO";
 import { useMemo } from "react";
 
 const useSx = () => useMemo(() => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
+  content: {
     alignItems: "center",
     justifyContent: "center",
     height: "100vh"
-  },
-  noMatch: {
-    fontFamily: "Azonix, sans-serif",
-    textAlign: "center",
-    mb: 4,
-    color: "primary.main"
-  },
-  noMatchMessage: {
-    textAlign: "center",
-    mb: 4
   }
 }), []);
 
@@ -28,17 +16,19 @@ const NotFoundPage = () => {
   const sx = useSx();
 
   return (
-    <Container component="main" sx={sx.root}>
+    <Container component="main">
       <SEO title="Not Found" />
-      <Typography variant="h1" sx={sx.noMatch}>
-        404
-      </Typography>
-      <Typography variant="h5" sx={sx.noMatchMessage}>
-        The page you were looking for does not exist.
-      </Typography>
-      <Button variant="contained" color="secondary" size="large" component={Link} to="/">
-        go back to home
-      </Button>
+      <Stack spacing={4} sx={sx.content}>
+        <Typography variant="h1">
+          404
+        </Typography>
+        <Typography variant="h5">
+          The page you were looking for does not exist.
+        </Typography>
+        <Button variant="contained" color="secondary" size="large" component={Link} to="/">
+          go back to home
+        </Button>
+      </Stack>
     </Container>
   );
 };
