fa0bb01cefddd437bff91629e0e8c2eb2b59bde3
Matthew Kwong <matthewkwong2@gmail.com>
2021-10-16 08:33:10 +0800

    Revert "[Test] Use MUI TextField"
    This reverts commit 09a087598b0a874ba5b3a738675d9db926ad8a7b.

--- src/features/Contact/ContactForm.js
+++ src/features/Contact/ContactForm.js
@@ -1,10 +1,10 @@
 import { ErrorRounded as Error, SendRounded as Send, CheckCircleRounded as Success } from "@mui/icons-material";
-import { Grid, TextField, useMediaQuery } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 import { isEmailValid, isValueEmpty } from "helpers";
 import { useRef, useState } from "react";
 
 import { LoadingButton } from "@mui/lab";
-// import TextField from "components/TextField";
+import TextField from "components/TextField";
 import { sendEmail } from "api";
 import useSx from "./useContactFormSx";
 
