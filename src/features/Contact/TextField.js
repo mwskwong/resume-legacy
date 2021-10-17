1812ee039cf0a083df08dfe5f6a2c0ca55896e7f
Matthew Kwong <matthewkwong2@gmail.com>
2021-10-17 16:48:36 +0800

    [Bug Fix] AIRA

--- src/features/Contact/TextField.js
+++ src/features/Contact/TextField.js
@@ -12,15 +12,17 @@ const TextField = forwardRef((
   ref
 ) => (
   <FormControl ref={ref} fullWidth={fullWidth} required={required} error={error}>
-    <InputLabel>{label}</InputLabel>
+    <InputLabel htmlFor={name}>{label}</InputLabel>
     <FilledInput
+      id={name}
       name={name}
       autoComplete={autoComplete}
       onChange={onChange}
       multiline={multiline}
       rows={rows}
+      aria-describedby={`${name}-helper-text`}
     />
-    <FormHelperText>{helperText}</FormHelperText>
+    <FormHelperText id={`${name}-helper-text`}>{helperText}</FormHelperText>
   </FormControl>
 ));
 
