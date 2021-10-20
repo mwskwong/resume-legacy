fd79596e8008c7e2a7230482c6ad17ea1a4f2276
kmw111 <kmw111@ha.org.hk>
2021-10-20 11:54:28 +0800

    [Optimization] Use lazy hydration... again

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Education from "features/Education";
 import Footer from "features/Footer";
 import { Fragment } from "react";
 import Landing from "features/Landing";
+import LazyHydrate from "react-lazy-hydration";
 import NavBar from "components/NavBar";
 import Project from "features/Project";
 import SEO from "components/SEO";
@@ -18,14 +19,28 @@ const IndexPage = () => {
       <NavBar />
       <main>
         <Landing />
-        <About />
-        <Testimonial />
-        <Work />
-        <Education />
-        <Project />
-        <Contact />
+        <LazyHydrate whenVisible>
+          <About />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Testimonial />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Work />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Education />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Project />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Contact />
+        </LazyHydrate>
       </main>
-      <Footer />
+      <LazyHydrate whenVisible>
+        <Footer />
+      </LazyHydrate>
       <ScrollToTopFab />
     </Fragment>
   );
