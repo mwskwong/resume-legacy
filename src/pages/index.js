3b9eb9c15577b3642dad8255ddcfbe4de9d3e8da
kmw111 <kmw111@ha.org.hk>
2021-10-20 18:32:29 +0800

    Revert "[Test] React 18"
    This reverts commit ab294ead0ffa961561cbe69cdc6b8cd28e205cae.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
-import { Fragment, Suspense, lazy } from "react";
-
-// import About from "features/About";
+import About from "features/About";
 import Contact from "features/Contact";
 import Education from "features/Education";
 import Footer from "features/Footer";
+import { Fragment } from "react";
 import Landing from "features/Landing";
+import LazyHydrate from "react-lazy-hydration";
 import NavBar from "components/NavBar";
 import Project from "features/Project";
 import SEO from "components/SEO";
 import ScrollToTopFab from "components/ScrollToTopFab";
 import Testimonial from "features/Testimonial";
 import Work from "features/Work";
-
-const About = lazy(() => import("features/About"));
 
 const IndexPage = () => {
   return (
@@ -21,14 +19,28 @@ const IndexPage = () => {
       <NavBar />
       <main>
         <Landing />
-        <About />
-        <Testimonial />
-        <Work />
-        <Education />
-        <Project />
-        <Contact />
+        <LazyHydrate whenVisible>
+          <About />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Testimonial />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Work />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Education />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Project />
+        </LazyHydrate>
+        <LazyHydrate whenVisible>
+          <Contact />
+        </LazyHydrate>
       </main>
-      <Footer />
+      <LazyHydrate whenVisible>
+        <Footer />
+      </LazyHydrate>
       <ScrollToTopFab />
     </Fragment>
   );
