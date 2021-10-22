a614c4a1043b1d9bc895a4117ec8e1dac4b084ff
kmw111 <kmw111@ha.org.hk>
2021-10-22 09:21:30 +0800

    [Refactor] Rename api and helpers to utils

--- src/helpers.js
+++
@@ -1,4 +0,0 @@
-const emailRegex = /^\w[\w.-]*@([\w-]+\.)+[\w-]+$/;
-
-export const isEmailValid = email => emailRegex.test(email);
-export const isValueEmpty = value => !value;
---
+++ src/utils/isEmpty.js
@@ -0,0 +1,3 @@
+const isEmpty = value => !value;
+
+export default isEmpty;
