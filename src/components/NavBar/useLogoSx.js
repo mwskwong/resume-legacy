05ee8a526be159b62f0e522dcdf004d691b0b59d
kmw111 <kmw111@ha.org.hk>
2021-10-28 10:18:54 +0800

    [Bug Fix] Prevent RubikVariable being used

--- src/components/NavBar/useLogoSx.js
+++ src/components/NavBar/useLogoSx.js
@@ -3,9 +3,8 @@ import { useMemo } from "react";
 const useSx = ({ sxProp }) => useMemo(() => ({
   root: sxProp,
   text: {
-    fontFamily: "Azonix, sans-serif",
     // Prevent RubikVariable being used
-    "@supports (font-variation-settings: normal)": "unset"
+    fontFamily: "Azonix, sans-serif !important"
   }
 }), [sxProp]);
 
