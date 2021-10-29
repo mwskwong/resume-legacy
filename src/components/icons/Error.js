f7a44a07ec69a579d3277d7b6fd0dee8b0e66532
kmw111 <kmw111@ha.org.hk>
2021-10-29 11:53:50 +0800

    [Bug FIx] icon color

--- src/components/icons/Error.js
+++ src/components/icons/Error.js
@@ -7,11 +7,11 @@ const Error = props => {
   return (
     <SvgIcon sx={sx.root} {...props}>
       <g transform="matrix(1.7142857142857142,0,0,1.7142857142857142,0,0)"><g>
-        <path d="M13,6.5a6,6,0,1,0-9.5,4.87V12.5a1,1,0,0,0,1,1h5a1,1,0,0,0,1-1V11.37A6,6,0,0,0,13,6.5Z" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></path>
-        <circle cx="4.5" cy="7" r="0.5" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></circle>
-        <circle cx="9.5" cy="7" r="0.5" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></circle>
-        <line x1="6" y1="11.5" x2="6" y2="13.5" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></line>
-        <line x1="8" y1="11.5" x2="8" y2="13.5" style="fill: none;stroke: #000000;stroke-linecap: round;stroke-linejoin: round"></line>
+        <path d="M13,6.5a6,6,0,1,0-9.5,4.87V12.5a1,1,0,0,0,1,1h5a1,1,0,0,0,1-1V11.37A6,6,0,0,0,13,6.5Z" style="fill: none;stroke: currentColor;stroke-linecap: round;stroke-linejoin: round"></path>
+        <circle cx="4.5" cy="7" r="0.5" style="fill: none;stroke: currentColor;stroke-linecap: round;stroke-linejoin: round"></circle>
+        <circle cx="9.5" cy="7" r="0.5" style="fill: none;stroke: currentColor;stroke-linecap: round;stroke-linejoin: round"></circle>
+        <line x1="6" y1="11.5" x2="6" y2="13.5" style="fill: none;stroke: currentColor;stroke-linecap: round;stroke-linejoin: round"></line>
+        <line x1="8" y1="11.5" x2="8" y2="13.5" style="fill: none;stroke: currentColor;stroke-linecap: round;stroke-linejoin: round"></line>
       </g></g>
     </SvgIcon>
   );
