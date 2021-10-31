9cff250ed269bc42783558a27ddb70f72c9ae856
Matthew Kwong <matthewkwong2@gmail.com>
2021-10-31 11:37:41 +0800

    [Optimization] Use lazy hydration

--- src/features/Testimonial/Testimonial.js
+++ src/features/Testimonial/Testimonial.js
@@ -3,6 +3,7 @@ import { Box, Container, Grid, Stack } from "@mui/material";
 import BrandingThemeProvider from "components/BrandingThemeProvider";
 import FunFacts from "./FunFacts";
 import HexSlice2 from "components/icons/HexSlice2";
+import LazyHydrate from "react-lazy-hydration";
 import SectionHeader from "components/SectionHeader";
 import { TESTIMONIAL } from "constants/nav";
 import loadable from "@loadable/component";
@@ -17,20 +18,22 @@ const Testimonial = () => {
   return (
     <BrandingThemeProvider mode="dark">
       <Box component="section" sx={sx.root} id={TESTIMONIAL.id}>
-        <Container>
-          <Stack spacing={6}>
-            <SectionHeader
-              heading="What People Say"
-              Icon={HexSlice2}
-            />
-            <div>
-              <Grid container spacing={6} sx={sx.contentGrid}>
-                <Carousel />
-                <FunFacts />
-              </Grid>
-            </div>
-          </Stack>
-        </Container>
+        <LazyHydrate whenVisible>
+          <Container>
+            <Stack spacing={6}>
+              <SectionHeader
+                heading="What People Say"
+                Icon={HexSlice2}
+              />
+              <div>
+                <Grid container spacing={6} sx={sx.contentGrid}>
+                  <Carousel />
+                  <FunFacts />
+                </Grid>
+              </div>
+            </Stack>
+          </Container>
+        </LazyHydrate>
       </Box>
     </BrandingThemeProvider>
   );
