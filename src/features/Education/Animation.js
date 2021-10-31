4d00f53a61c049971011362bf5fb322a60c243c2
Matthew Kwong <matthewkwong2@gmail.com>
2021-10-31 12:14:35 +0800

    [QoL] Minor changes

--- src/features/Education/Animation.js
+++ src/features/Education/Animation.js
@@ -1,8 +1,11 @@
-import ArcticOceanFractal from "arctic-ocean-fractal";
+import loadable from "@loadable/component";
 import { useInView } from "react-intersection-observer";
+
+const ArcticOceanFractal = loadable(() => import("arctic-ocean-fractal"), { ssr: false });
 
 const Animation = () => {
   const { ref, inView } = useInView({ triggerOnce: true });
+
   return (
     <div ref={ref}>
       <ArcticOceanFractal pose={inView ? "show" : "hide"} />
