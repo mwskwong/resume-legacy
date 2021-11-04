9d6204f15e55fc21c273b883680c5c86ffec2f64
kmw111 <kmw111@ha.org.hk>
2021-11-04 13:26:38 +0800

    Revert "[Bug Fix] Navigator not definded in client side"
    This reverts commit 32ce06813335680234b1159b6e71ffd225ecf22a.

--- src/brandingTheme/ssrMatchMedia.js
+++ src/brandingTheme/ssrMatchMedia.js
@@ -1,7 +1,7 @@
 import mediaQuery from "css-mediaquery";
 import parser from "ua-parser-js";
 
-const deviceType = (navigator && parser(navigator.userAgent).device.type) || "desktop";
+const deviceType = parser(navigator.userAgent).device.type || "desktop";
 const ssrMatchMedia = query => ({
   matches: mediaQuery.match(query, {
     width: deviceType === "mobile" ? "0px" : "1024px"
