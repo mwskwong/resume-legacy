45e25ef2baf5988c55da2f35734d942aaf899e30
Matthew Kwong <matthewkwong2@gmail.com>
2021-11-07 10:48:17 +0800

    [Feature] Use default runtimeCaching

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,31 +90,7 @@ module.exports = {
       resolve: "gatsby-plugin-offline",
       options: {
         workboxConfig: {
-          globPatterns: ["**icon-*"],
-          runtimeCaching: [
-            {
-              urlPattern: /(\.js$|\.css$|static\/)/,
-              handler: "CacheFirst"
-            },
-            {
-              urlPattern: /^https?:.*\/page-data\/.*\.json/,
-              handler: "NetworkFirst",
-              options: {
-                networkTimeoutSeconds: 3
-              }
-            },
-            {
-              urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-              handler: "StaleWhileRevalidate"
-            },
-            {
-              urlPattern: /\/$/,
-              handler: "NetworkFirst",
-              options: {
-                networkTimeoutSeconds: 3
-              }
-            }
-          ]
+          globPatterns: ["**icon-*"]         
         }
       }
     },
