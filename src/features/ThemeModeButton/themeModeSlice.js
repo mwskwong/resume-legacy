54076ffdb0e889a5259463b8baf09769c83bc331
Matthew Kwong <matthewkwong2@gmail.com>
2021-11-07 14:31:30 +0800

    [Bug Fix] Inconsistant theme mode

--- src/features/ThemeModeButton/themeModeSlice.js
+++ src/features/ThemeModeButton/themeModeSlice.js
@@ -1,17 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const prefersDarkMode = typeof window !== "undefined" && window.matchMedia("(prefers-color-scheme: dark)");
-
 const themeModeSlice = createSlice({
   name: "themeMode",
-  initialState: prefersDarkMode ? "dark" : "light",
+  initialState: "light",
   reducers: {
-    toggleThemeMode: state => state === "light" ? "dark" : "light"
+    toggleThemeMode: state => state === "light" ? "dark" : "light",
+    setThemeMode: (_, action) => action.payload
   }
 });
 
 const { actions, reducer } = themeModeSlice;
-export const { toggleThemeMode } = actions;
+export const { toggleThemeMode, setThemeMode } = actions;
 export default reducer;
 
 export const selectThemeMode = state => state.themeMode;
--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,13 +1,20 @@
+import { lazy, useEffect } from "react";
+import { selectThemeMode, setThemeMode } from "features/ThemeModeButton/themeModeSlice";
+
 import BrandingThemeProvider from "components/BrandingThemeProvider";
 import { CssBaseline } from "@mui/material";
 import PropTypes from "prop-types";
-import { lazy } from "react";
-import { selectThemeMode } from "features/ThemeModeButton/themeModeSlice";
 import { useSelector } from "react-redux";
+
 const PWASnackbar = lazy(() => import(/* webpackChunkName: "pwa-snackbar" */ "features/PWASnackbar"));
 
 const Layout = ({ children }) => {
   const themeMode = useSelector(selectThemeMode);
+
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+    setThemeMode(prefersDarkMode ? "dark" : "light");
+  }, []);
 
   return (
     <BrandingThemeProvider mode={themeMode}>
