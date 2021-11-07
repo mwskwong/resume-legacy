0f070068d6c7f7f76cc0302c6ac812277d3ca136
Matthew Kwong <matthewkwong2@gmail.com>
2021-11-07 14:42:32 +0800

    [Bug Fix] missing dispatch

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,19 +1,20 @@
 import { lazy, useEffect } from "react";
 import { selectThemeMode, setThemeMode } from "features/ThemeModeButton/themeModeSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 import BrandingThemeProvider from "components/BrandingThemeProvider";
 import { CssBaseline } from "@mui/material";
 import PropTypes from "prop-types";
-import { useSelector } from "react-redux";
 
 const PWASnackbar = lazy(() => import(/* webpackChunkName: "pwa-snackbar" */ "features/PWASnackbar"));
 
 const Layout = ({ children }) => {
   const themeMode = useSelector(selectThemeMode);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-    setThemeMode(prefersDarkMode ? "dark" : "light");
+    dispatch(setThemeMode(prefersDarkMode ? "dark" : "light"));
   }, []);
 
   return (
