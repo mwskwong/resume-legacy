117defa3cb1ee3ab53e0b55f9b73472aa9892821
Matthew Kwong <matthewkwong2@gmail.com>
2021-11-09 23:11:56 +0800

    [Feature] Use default setting of gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,45 +88,16 @@ module.exports = {
         }
       }
     },
-    {
-      resolve: "gatsby-plugin-offline",
-      options: {
-        workboxConfig: {
-          globPatterns: ["**icon-*"],
-          runtimeCaching: [
-            {
-              urlPattern: /(\.js$|\.css$|static\/)/,
-              handler: "CacheFirst"
-            },
-            {
-              urlPattern: /^https?:.*\/page-data\/.*\.json/,
-              handler: "NetworkFirst",
-              options: {
-                networkTimeoutSeconds: 3
-              }
-            },
-            {
-              urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-              handler: "StaleWhileRevalidate"
-            },
-            {
-              urlPattern: /\/$/,
-              handler: "NetworkFirst",
-              options: {
-                networkTimeoutSeconds: 3
-              }
-            }
-          ]
-        }
-      }
-    },
-    ...(
-      ANALYZE_BUNDLE
-        ? [
-          "gatsby-plugin-webpack-bundle-analyser-v2",
-          "gatsby-plugin-perf-budgets"
-        ]
-        : []
-    )
+    "gatsby-plugin-offline",
+    ANALYZE_BUNDLE && "gatsby-plugin-webpack-bundle-analyser-v2",
+    ANALYZE_BUNDLE && "gatsby-plugin-perf-budgets"
+    // ...(
+    //   ANALYZE_BUNDLE
+    //     ? [
+    //       "gatsby-plugin-webpack-bundle-analyser-v2",
+    //       "gatsby-plugin-perf-budgets"
+    //     ]
+    //     : []
+    // )
   ]
 };
