c0411721cf0d494bf49aabeb93a335266c999e8b
kmw111 <kmw111@ha.org.hk>
2021-11-10 09:05:50 +0800

    [Feature] Use both gatsby-plugin-emotion and gatsby-plugin-material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,8 @@ module.exports = {
         spaceId: "zz9cwhc5t97i"
       }
     },
-    "gatsby-plugin-mui",
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-material-ui",
     "gatsby-plugin-image",
     {
       resolve: "gatsby-plugin-sharp",
