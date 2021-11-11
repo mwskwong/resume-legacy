f255c468e4626bd57fc61fbfba278a3c4fcabf0a
kmw111 <kmw111@ha.org.hk>
2021-11-11 10:55:11 +0800

    Revert "[Optimization] Prevent layout shift"
    This reverts commit 9e768a2fb2d6e68909942df5ed88dc68fecaf45b.

--- plugins/gatsby-plugin-mui/gatsby-ssr.js
+++ plugins/gatsby-plugin-mui/gatsby-ssr.js
@@ -1,29 +1,29 @@
 import { CacheProvider } from "@emotion/react";
 import { createEmotionCache } from "./createEmotionCache";
-import createEmotionServer from "@emotion/server/create-instance";
-import { renderToString } from "react-dom/server";
 
-export const replaceRenderer = ({ bodyComponent, setHeadComponents, replaceBodyHTMLString }) => {
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks } = createEmotionServer(cache);
+// export const replaceRenderer = ({ bodyComponent, setHeadComponents, replaceBodyHTMLString }) => {
+//   const cache = createEmotionCache();
+//   const { extractCriticalToChunks } = createEmotionServer(cache);
 
-  const emotionStyles = extractCriticalToChunks(
-    renderToString(
-      <CacheProvider value={cache}>
-        {bodyComponent}
-      </CacheProvider>
-    )
-  );
+//   const emotionStyles = extractCriticalToChunks(
+//     renderToString(<CacheProvider value={cache}>{bodyComponent}</CacheProvider>)
+//   );
 
-  setHeadComponents(
-    emotionStyles.styles.map((style) => (
-      <style
-        data-emotion={`${style.key} ${style.ids.join(" ")}`}
-        key={style.key}
-        dangerouslySetInnerHTML={{ __html: style.css }}
-      />
-    ))
-  );
+//   setHeadComponents(
+//     emotionStyles.styles.map((style) => (
+//       <style
+//         data-emotion={`${style.key} ${style.ids.join(" ")}`}
+//         key={style.key}
+//         dangerouslySetInnerHTML={{ __html: style.css }}
+//       />
+//     ))
+//   );
 
-  replaceBodyHTMLString(emotionStyles.html);
-};
+//   replaceBodyHTMLString(emotionStyles.html);
+// };
+
+export const wrapRootElement = ({ element }) => (
+  <CacheProvider value={createEmotionCache()}>
+    {element}
+  </CacheProvider>
+);
