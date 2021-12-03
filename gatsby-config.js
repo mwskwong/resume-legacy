05ffb95088023bc48280f057dae76eda982d377e
kmw111 <kmw111@ha.org.hk>
2021-12-03 09:41:19 +0800

    [Featrue] Use COntentful preview API for DEV and `next` branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,10 @@ require("dotenv").config({
 });
 
 const { CF_PAGES_BRANCH = "main", CONTENTFUL_ACCESS_TOKEN, ANALYZE_BUNDLE, NODE_ENV } = process.env;
-const isProd = CF_PAGES_BRANCH === "main";
+const prod = CF_PAGES_BRANCH === "main";
 const PROD_URL = "https://mwskwong.com";
 const PREVIEW_URL = `https://${CF_PAGES_BRANCH}.mwskwong.com`;
-const siteUrl = isProd ? PROD_URL : PREVIEW_URL;
+const siteUrl = prod ? PROD_URL : PREVIEW_URL;
 
 module.exports = {
   jsxRuntime: "automatic",
@@ -23,6 +23,7 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
+        host: NODE_ENV === "development" || !prod ? "preview.contentful.com" : "cdn.contentful.com",
         accessToken: CONTENTFUL_ACCESS_TOKEN,
         spaceId: "zz9cwhc5t97i"
       }
@@ -70,7 +71,7 @@ module.exports = {
             "Link: <https://images.ctfassets.net>; rel=preconnect"
           ],
           ...(
-            isProd
+            prod
               ? {
                 "https://:project.pages.dev/*": [
                   `Link: <${PROD_URL}/:splat>; rel=canonical`
