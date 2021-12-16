0317ca4d9f5e724b5dc77ee054978c7a05d96cb2
kmw111 <kmw111@ha.org.hk>
2021-12-16 08:36:09 +0800

    [QoL] Replace `||` with `??`

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,7 +2,7 @@ import emailjs from "./emailjs";
 
 const sendEmail = emailData => emailjs.send("personal_email", "personal_webpage", {
   ...emailData,
-  message: emailData.message.split(/\r?\n/g).map(line => line || "\u200b")
+  message: emailData.message.split(/\r?\n/g).map(line => line ?? "\u200b")
 });
 
 export default sendEmail;
