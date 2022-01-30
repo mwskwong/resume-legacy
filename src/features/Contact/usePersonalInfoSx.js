a4216f2c0f8991af3d0e13a79bfd02cbe5fb5a24
Matthew Kwong <matthewkwong2@gmail.com>
2022-01-30 09:06:57 +0800

    [Feature] Change `SkillSet` colors; Merge `PersonalInfo`

--- src/features/Contact/usePersonalInfoSx.js
+++ src/features/Contact/usePersonalInfoSx.js
@@ -3,6 +3,20 @@ import { useMemo } from "react";
 const useSx = () => useMemo(() => ({
   root: {
     p: 3
+  },
+  itemContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  title: {
+    color: "primary.main",
+    textAlign: "center",
+    textTransform: "capitalize",
+    mt: 2
+  },
+  value: {
+    textAlign: "center"
   }
 }), []);
 
