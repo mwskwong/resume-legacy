810e0ef51cdf380b279a5892bcb76bfba21982fc
Matthew Kwong <matthewkwong2@gmail.com>
2022-02-23 08:39:28 +0800

    [Bug FIx] icon path

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,23 +97,23 @@ module.exports = {
         start_url: ".",
         icons: [
           {
-            src: "icon-192x192.png",
+            src: "/icon-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "icon-512x512.png",
+            src: "/icon-512x512.png",
             sizes: "512x512",
             type: "image/png"
           },
           {
-            src: "icon-maskable-192x192.png",
+            src: "/icon-maskable-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "maskable"
           },
           {
-            src: "icon-maskable-512x512.png",
+            src: "/icon-maskable-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable"
