95bb03f1a453da27af820920a0358bee75e008d5
Matthew Kwong <matthewkwong2@gmail.com>
2022-04-11 20:03:41 +0800

    Enable About

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,7 @@
-// import About from "features/About";
-// import Contact from "features/Contact";
-// import Education from "features/Education";
-// import Testimonial from "features/Testimonial";
-// import Work from "features/Work";
-
+import About from "features/About";
 import Footer from "features/Footer";
 import FooterDivider from "components/dividers/Footer";
 import { Fragment } from "react";
-import Landing from "features/Landing";
 import NavBar from "components/NavBar";
 import SEO from "components/SEO";
 import ScrollToTopFab from "components/ScrollToTopFab";
@@ -16,6 +10,24 @@ import WaveRough from "components/dividers/WaveRough";
 import WaveSmooth from "components/dividers/WaveSmooth";
 import WaveSmooth2 from "components/dividers/WaveSmooth2";
 import WaveSmoothFlat from "components/dividers/WaveSmoothFlat";
+
+// import Contact from "features/Contact";
+// import Education from "features/Education";
+// import Testimonial from "features/Testimonial";
+// import Work from "features/Work";
+// import Landing from "features/Landing";
+
+
+
+
+
+
+
+
+
+
+
+
 
 const bgcolors = {
   landing: "default",
@@ -73,9 +85,9 @@ const IndexPage = () => {
       <SEO />
       <NavBar />
       <main>
-        <Landing />
+        {/* <Landing /> */}
         <WaveSmooth sx={sx.waveSmooth} />
-        {/* <About sx={sx.about} /> */}
+        <About sx={sx.about} />
         <WaveRough sx={sx.waveRough} />
         {/* <Testimonial /> */}
         <WaveSmoothFlat sx={sx.waveSmoothFlat} />
