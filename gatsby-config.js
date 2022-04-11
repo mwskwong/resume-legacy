408accdd9176aee69ef2bdae6e9186d1acc1c41f
Matthew Kwong <matthewkwong2@gmail.com>
2022-04-11 15:41:49 +0800

    Revert "Replace `react-helmet` with `react-helmet-async`"
    This reverts commit c1f0a016241be33d6bc9a3f9c90e7caf8c181b42.

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
       }
     },
     "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet-async",
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-sitemap",
       options: {
