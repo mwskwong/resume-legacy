652cb571dc45fe2d28f0c806cd68ccfe6d8d0815
Matthew Kwong <matthewkwong2@gmail.com>
2022-04-11 14:35:30 +0800

    Revert "[Test] Disable useMediaQuery"
    This reverts commit 7e3448a98fe81d08eb0ad4fcef096227a34c628c.

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,14 +1,13 @@
 import { Timeline as MuiTimeline } from "@mui/lab";
 import PropTypes from "prop-types";
 import TimelineItem from "./TimelineItem";
-
-// import { useMediaQuery } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 
 const Timeline = ({ data }) => {
-  // const mdDown = useMediaQuery(theme => theme.breakpoints.down("md"));
+  const mdDown = useMediaQuery(theme => theme.breakpoints.down("md"));
 
   return (
-    <MuiTimeline position="right">
+    <MuiTimeline position={mdDown ? "right" : "alternate"}>
       {data.map((datum, index) => (
         <TimelineItem key={index} data={datum} />
       ))}
