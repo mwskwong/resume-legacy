c1f0a016241be33d6bc9a3f9c90e7caf8c181b42
Matthew Kwong <matthewkwong2@gmail.com>
2022-04-11 15:30:49 +0800

    Replace `react-helmet` with `react-helmet-async`

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
       }
     },
     "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-react-helmet-async",
     {
       resolve: "gatsby-plugin-sitemap",
       options: {
