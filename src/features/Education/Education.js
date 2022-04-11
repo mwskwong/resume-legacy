7353aa0dd019704035ec6f46aec630f3407608b2
Matthew Kwong <matthewkwong2@gmail.com>
2022-04-11 13:40:44 +0800

    [Bug Fix] `ArcticOceanFractal` import path

--- src/features/Education/Education.js
+++ src/features/Education/Education.js
@@ -8,7 +8,7 @@ import PropTypes from "prop-types";
 import SectionHeader from "components/SectionHeader";
 import useSx from "./useEducationSx";
 
-const ArcticOceanFractal = lazy(() => import("./ArcticOceanFractal"));
+const ArcticOceanFractal = lazy(() => import("components/ArcticOceanFractal"));
 
 const Education = ({ sx: sxProp }) => {
   const sx = useSx({ sxProp });
