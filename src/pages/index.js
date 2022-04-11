b9a9028279cf0ff588b4cd91d8d5dad4e2ebe5c4
Matthew Kwong <matthewkwong2@gmail.com>
2022-04-11 20:04:17 +0800

    Enable Wrk

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 // import About from "features/About";
 // import Contact from "features/Contact";
 // import Education from "features/Education";
+// import Testimonial from "features/Testimonial";
 
 import Footer from "features/Footer";
 import FooterDivider from "components/dividers/Footer";
@@ -8,14 +9,13 @@ import { Fragment } from "react";
 import NavBar from "components/NavBar";
 import SEO from "components/SEO";
 import ScrollToTopFab from "components/ScrollToTopFab";
-import Testimonial from "features/Testimonial";
 import WaveRoaring from "components/dividers/WaveRoaring";
 import WaveRough from "components/dividers/WaveRough";
 import WaveSmooth from "components/dividers/WaveSmooth";
 import WaveSmooth2 from "components/dividers/WaveSmooth2";
 import WaveSmoothFlat from "components/dividers/WaveSmoothFlat";
+import Work from "features/Work";
 
-// import Work from "features/Work";
 // import Landing from "features/Landing";
 
 
@@ -90,9 +90,9 @@ const IndexPage = () => {
         <WaveSmooth sx={sx.waveSmooth} />
         {/* <About sx={sx.about} /> */}
         <WaveRough sx={sx.waveRough} />
-        <Testimonial />
+        {/* <Testimonial /> */}
         <WaveSmoothFlat sx={sx.waveSmoothFlat} />
-        {/* <Work sx={sx.work} /> */}
+        <Work sx={sx.work} />
         <WaveSmooth2 sx={sx.waveSmooth2} />
         {/* <Education sx={sx.education} /> */}
         <WaveRoaring sx={sx.waveRoaring} />
