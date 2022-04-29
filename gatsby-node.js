240ad8a6e33096f01ada9acf0e669971fced2a34
kmw111 <kmw111@ha.org.hk>
2022-04-29 12:27:09 +0800

    [Test] Prevent base64 encoding

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,14 @@ const path = require("path");
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif|webp|avif|woff2?)$/i,
+          type: "asset/resource"
+        }
+      ]
+    },
     resolve: {
       modules: [path.resolve(__dirname, "src"), "node_modules"]
     }
