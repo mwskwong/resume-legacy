5794d10c986ce1815607dda3aa8e87770dcaa57c
kmw111 <kmw111@ha.org.hk>
2022-04-29 14:26:22 +0800

    Revert

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,19 +2,6 @@ const path = require("path");
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
-    module: {
-      rules: [
-        {
-          test: /\.(png|jpe?g|gif|webp|avif|woff2?)$/i,
-          type: "asset",
-          parser: {
-            dataUrlCondition: {
-              maxSize: 1
-            }
-          }
-        }
-      ]
-    },
     resolve: {
       modules: [path.resolve(__dirname, "src"), "node_modules"]
     }
