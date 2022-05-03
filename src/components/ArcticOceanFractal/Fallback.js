607c24d7917be15bd44b308746fe83258023b31c
Matthew Kwong <matthewkwong2@gmail.com>
2022-05-04 00:30:44 +0800

    [Optimization] Lazy load `ArcticOceanFractal`

---
+++ src/components/ArcticOceanFractal/Fallback.js
@@ -0,0 +1,6 @@
+import { Box } from "@mui/material";
+
+const Fallback = () => <Box sx={{ width: "100%", aspectRatio: "800 / 640.66" }} />;
+
+Fallback.whyDidYouRender = true;
+export default Fallback;
