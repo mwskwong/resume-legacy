f4e8ffcc5b29614910072ff96add597b10c350dc
kmw111 <kmw111@ha.org.hk>
2022-05-05 17:50:23 +0800

    [Feature] Replace `react-helmet` with `react-helmet-async`

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -1,6 +1,6 @@
 import { graphql, useStaticQuery } from "gatsby";
 
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import PropTypes from "prop-types";
 import { memo } from "react";
 
