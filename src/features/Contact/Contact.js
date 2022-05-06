fc982a99771adb115e94357d2793fce5174ff03a
kmw111 <kmw111@ha.org.hk>
2022-05-06 15:53:28 +0800

    Rollback to use `gatsby-plugin-material-ui`

--- src/features/Contact/Contact.js
+++ src/features/Contact/Contact.js
@@ -42,13 +42,23 @@ const Contact = ({ sx: sxProp }) => {
 
   };
 
+  // Workaround: Grid container with children in different sizes causing the default style to be missing in SSR
+  const gridContainerStyle = {
+    boxSizing: "border-box",
+    display: "flex",
+    flexFlow: "row wrap",
+    marginTop: -60,
+    width: "calc(100% + 60px)",
+    marginLeft: -60
+  };
+
   return (
     <Box sx={sx.root} component="section" id={CONTACT.id}>
       <Container>
         <Stack spacing={6}>
           <SectionHeader heading="Get In Touch" />
           <form onSubmit={handleSubmit(handleFormSubmit)}>
-            <Grid container spacing={6}>
+            <Grid container spacing={6} style={gridContainerStyle}>
               <Grid item md={4} xs={12}>
                 <PersonalInfo />
               </Grid>
