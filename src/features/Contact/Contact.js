494bd99189221e5e3c021dc85fc1a2d347e4fe08
Matthew Kwong <matthewkwong2@gmail.com>
2022-05-13 23:08:46 +0800

    [Optimization] Use both `gatsby-plugin-emotion` and `gatsby-plugin-material-ui`

--- src/features/Contact/Contact.js
+++ src/features/Contact/Contact.js
@@ -1,5 +1,3 @@
-import * as styles from "./contact.module.css";
-
 import { Box, Container, Grid, Stack, useMediaQuery } from "@mui/material";
 import { ErrorRounded as Error, SendRounded as Send, CheckCircleRounded as Success } from "@mui/icons-material";
 import React, { memo, useState } from "react";
@@ -49,7 +47,7 @@ const Contact = ({ sx: sxProp }) => {
         <Stack spacing={6}>
           <SectionHeader heading="Get In Touch" />
           <form onSubmit={handleSubmit(handleFormSubmit)}>
-            <Grid container spacing={6} className={styles.gridContainer}>
+            <Grid container spacing={6}>
               <Grid item md={4} xs={12}>
                 <PersonalInfo />
               </Grid>
