b5e3f9a930efc9179c871ad81e2cac2e30a01e00
Matthew Kwong <matthewkwong2@gmail.com>
2022-05-15 08:35:59 +0800

    [Feature] Lazy load componenets

--- src/features/Hero/Hero.js
+++ src/features/Hero/Hero.js
@@ -1,12 +1,13 @@
 import { Box, Container } from "@mui/material";
-import React, { memo } from "react";
+import React, { Suspense, lazy, memo } from "react";
 
-import ArcticLandscape from "components/illustrations/ArcticLandscape";
 import DownloadResumeButton from "./DownloadResumeButton";
 import { HOME } from "constants/nav";
 import SocialMedia from "components/SocialMedia";
 import Title from "./Title";
 import useSx from "./useHeroSx";
+
+const ArcticLandscape = lazy(() => import("components/illustrations/ArcticLandscape"));
 
 const Hero = () => {
   const sx = useSx();
@@ -14,7 +15,9 @@ const Hero = () => {
   return (
     <Container component="section" sx={sx.container} id={HOME.id}>
       <Box sx={sx.animationWrapper}>
-        <ArcticLandscape />
+        <Suspense>
+          <ArcticLandscape />
+        </Suspense>
       </Box>
       <Title />
       <SocialMedia sx={sx.socialMedia} />
