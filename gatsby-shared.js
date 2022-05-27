71ea11e2ccddbf291601a8d43769e25eafb52bdc
mwskwong <matthewkwong2@gmail.com>
2022-05-27 11:04:08 +0800

    Revert "[Optimization] Load `beacon` script with Script API"
    This reverts commit c839bc5863da915a0b1fcbb255321e19fbc75316.

--- gatsby-shared.js
+++ gatsby-shared.js
@@ -1,7 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React, { StrictMode } from "react";
 
-import { Script } from "gatsby";
 import brandingTheme from "brandingTheme";
 
 export const wrapRootElement = ({ element }) => (
@@ -10,10 +9,5 @@ export const wrapRootElement = ({ element }) => (
       <CssBaseline enableColorScheme />
       {element}
     </ThemeProvider>
-    <Script
-      src="https://static.cloudflareinsights.com/beacon.min.js"
-      data-cf-beacon={"{\"token\": \"79283f6990f2443f9864dcf4e424fa30\"}"}
-      strategy="off-main-thread"
-    />
   </StrictMode>
 );
