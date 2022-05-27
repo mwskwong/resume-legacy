c4b297d2c53728d221405c544c0b550ff0b7b689
mwskwong <matthewkwong2@gmail.com>
2022-05-27 09:54:13 +0800

    [Revert] Remove `Script`

--- gatsby-shared.js
+++ gatsby-shared.js
@@ -1,7 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import React, { StrictMode } from "react";
 
-import { Script } from "gatsby";
 import brandingTheme from "brandingTheme";
 
 export const wrapRootElement = ({ element }) => (
@@ -10,10 +9,5 @@ export const wrapRootElement = ({ element }) => (
       <CssBaseline enableColorScheme />
       {element}
     </ThemeProvider>
-    <Script
-      src="https://static.cloudflareinsights.com/beacon.min.js"
-      data-cf-beacon={"{\"token\": \"3b6f8c2bc3c7427bad1e76e99f56fdcb\"}"}
-      strategy="off-main-thread"
-    />
   </StrictMode>
 );
