5ae765bb39fc32a709923d656f68beb1c07ddb37
mwskwong <matthewkwong2@gmail.com>
2022-05-27 15:12:57 +0800

    [Feature] Add EnterpriseDB icon

---
+++ src/components/icons/EnterpriseDB.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { SvgIcon } from "@mui/material";
+import { siGithub } from "simple-icons/icons";
+import useSx from "./useThirdPartyIconSx";
+
+const GitHub = props => {
+  const sx = useSx();
+
+  return (
+    <SvgIcon sx={sx.simpleIcons} {...props}>
+      <path d={siGithub.path} />
+    </SvgIcon>
+  );
+};
+
+GitHub.muiName = SvgIcon.muiName;
+GitHub.whyDidYouRender = true;
+
+export default GitHub;
