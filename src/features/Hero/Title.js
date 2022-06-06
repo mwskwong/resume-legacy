21770a872881680d2fff1cf7a7e2ba84860548c9
mwskwong <matthewkwong2@gmail.com>
2022-06-06 11:54:28 +0800

    [Test] SEO

--- src/features/Hero/Title.js
+++ src/features/Hero/Title.js
@@ -1,5 +1,5 @@
 import { Box, Typography } from "@mui/material";
-import React, { useMemo } from "react";
+import React, { Fragment, useMemo } from "react";
 import { graphql, useStaticQuery } from "gatsby";
 
 import TypeIt from "typeit-react";
@@ -19,20 +19,29 @@ const Title = () => {
     }
   }`);
 
-  const typeItOptions = useMemo(() => ({
-    strings: [
-      `${name.firstName} ${name.lastName}.`,
-      ...occupationNodes.nodes.map(({ title }) => `A ${title}.`)
-    ],
+  const strings = useMemo(() => [
+    `${name.firstName} ${name.lastName}.`,
+    ...occupationNodes.nodes.map(({ title }) => `A ${title}.`)
+  ], [name.firstName, name.lastName, occupationNodes.nodes]);
+
+  const typeItOptions = {
     startDelete: true,
+    breakLines: false,
     loop: true
-  }), [name.firstName, name.lastName, occupationNodes.nodes]);
+  };
 
   return (
     <Box sx={sx.root}>
       <Typography sx={sx.title} variant="h1">
         {"I Am "}
-        <TypeIt options={typeItOptions} />
+        <TypeIt options={typeItOptions}>
+          {strings.map((string, index) => (
+            <Fragment key={index}>
+              {string}
+              <br />
+            </Fragment>
+          ))}
+        </TypeIt>
       </Typography>
     </Box>
   );
