7d8a16733210fc36357dcc0b34e1e2383018e667
mwskwong <matthewkwong2@gmail.com>
2022-06-08 09:20:02 +0800

    Lazy load `Carousel`

---
+++ src/features/Testimonial/CarouselFallback.js
@@ -0,0 +1,13 @@
+import { Box, Grid } from "@mui/material";
+
+import React from "react";
+
+const CarouselFallback = () => (
+  <Grid item md={6} xs={12}>
+    <Box sx={{ height: { xs: 298, sm: 202, md: 274, lg: 226 } }} />
+  </Grid>
+);
+
+CarouselFallback.whyDidYouRender = true;
+
+export default CarouselFallback;
