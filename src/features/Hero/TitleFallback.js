94595828b351997b118830f4f69265c1ed757723
mwskwong <matthewkwong2@gmail.com>
2022-06-08 09:17:53 +0800

    [Bug Fix] `TitleFallback` height

--- src/features/Hero/TitleFallback.js
+++ src/features/Hero/TitleFallback.js
@@ -4,13 +4,13 @@ import React from "react";
 const TitleFallback = () => (
   <Box
     sx={{
-      height: theme => `${theme.typography.h1.lineHeight}em`,
       "@media (max-width: 742px)": {
         height: theme => `${theme.typography.h1.lineHeight * 2}em`
       },
       "@media (max-width: 370px)": {
         height: theme => `${theme.typography.h1.lineHeight * 3}em`
-      }
+      },
+      height: theme => `${theme.typography.h1.lineHeight}em`
     }}
   />
 );
