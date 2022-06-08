83772f4224c7c2fc8884e571067081a8863b3238
mwskwong <matthewkwong2@gmail.com>
2022-06-08 11:29:52 +0800

    [Bug Fix] Link header format

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,7 @@ module.exports = {
       resolve: "gatsby-plugin-netlify",
       options: {
         allPageHeaders: [
-          "Link: https://static.cloudflareinsights.com; rel=preconnect"
+          "Link: <https://static.cloudflareinsights.com>; rel=preconnect"
         ],
         headers: {
           "/*": [
