f1afcd41920e929466d2cd7937f2063425f9b961
mwskwong <matthewkwong2@gmail.com>
2022-06-17 11:04:55 +0800

    feat: Remove SW

--- gatsby-config.js
+++ gatsby-config.js
@@ -143,17 +143,18 @@ module.exports = {
         }
       }
     },
-    {
-      resolve: "gatsby-plugin-offline",
-      options: {
-        workboxConfig: {
-          globPatterns: [
-            "**/icon-*",
-            "**/favicon.ico"
-          ]
-        }
-      }
-    },
+    "gatsby-plugin-remove-serviceworker",
+    // {
+    //   resolve: "gatsby-plugin-offline",
+    //   options: {
+    //     workboxConfig: {
+    //       globPatterns: [
+    //         "**/icon-*",
+    //         "**/favicon.ico"
+    //       ]
+    //     }
+    //   }
+    // },
     ANALYZE_BUNDLE && "gatsby-plugin-webpack-bundle-analyser-v2",
     ANALYZE_BUNDLE && "gatsby-plugin-perf-budgets"
   ].filter(Boolean)
