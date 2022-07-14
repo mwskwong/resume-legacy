2a54ab699d31549855ac647f1d1bb67fbcec8a90
Matthew Kwong <matthewkwong2@gmail.com>
2022-07-14 15:06:56 +0800

    fix: Use `require` when importing WDYR

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,20 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 import { GatsbyBrowser } from "gatsby";
 
 export { wrapRootElement } from "./gatsby-shared";
 
 export const onClientEntry: GatsbyBrowser["onClientEntry"] = () => {
   if (process.env.NODE_ENV === "development") {
-    (async () => {
-      const [React, { default: whyDidYouRender }] = await Promise.all([
-        import("react"),
-        import("@welldone-software/why-did-you-render")
-      ]);
+    const React = require("react");
+    const whyDidYouRender = require("@welldone-software/why-did-you-render");
 
-      whyDidYouRender(React, {
-        trackAllPureComponents: true,
-        exclude: [/RouterImpl/, /SideEffect\(NullComponent\)/]
-      });
-    })();
+    whyDidYouRender(React, {
+      trackAllPureComponents: true,
+      exclude: [/RouterImpl/, /SideEffect\(NullComponent\)/]
+    });
   }
 };
 
