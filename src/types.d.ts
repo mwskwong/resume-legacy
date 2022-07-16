538ed7ceb2afbc5aefc724944c746f2a59716164
Matthew Kwong <matthewkwong2@gmail.com>
2022-07-16 09:51:56 +0800

    feat: Migrate index page to TS

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,7 +2,7 @@ import { SxProps, Theme } from "@mui/material";
 
 export type SectionId = "home" | "about" | "work" | "education" | "testimonial" | "contact"
 
-export type SectionProps = { sx: SxProps<Theme> }
+export type SectionProps = { sx?: SxProps<Theme> }
 
 export type UseSx = (sx?: SxProps<Theme>) => {
   [key: string]: SxProps<Theme> | undefined
