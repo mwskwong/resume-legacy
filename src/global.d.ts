be0714d7d541171d226e66d4ef050abbbf4a6fd2
Matthew Kwong <matthewkwong2@gmail.com>
2022-07-18 09:59:26 +0800

    feat: type check `process.env`

--- src/global.d.ts
+++ src/global.d.ts
@@ -19,3 +19,17 @@ declare module "react" {
     muiName?: string
   }
 }
+
+// Define process.env
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      CF_PAGES: 1 | 0,
+      CF_PAGES_COMMIT_SHA: string,
+      CF_PAGES_BRANCH: string,
+      CF_PAGES_URL: string,
+      CONTENTFUL_ACCESS_TOKEN: string,
+      ANALYZE_BUNDLE: 1 | 0
+    }
+  }
+}
