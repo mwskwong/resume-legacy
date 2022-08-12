ba29e1c4a8c2f1797f085bfc3035abe350b2e71d
Matthew Kwong <matthewkwong2@gmail.com>
2022-08-12 12:38:49 +0800

    Revert "fix: Update cloudflareinsights script token"
    This reverts commit 923abdfb8d822395b68f8b1dc53da854eb4cba48.

--- gatsby-shared.tsx
+++ gatsby-shared.tsx
@@ -12,7 +12,7 @@ export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({ element }) =
     </ThemeProvider>
     <Script
       src="https://static.cloudflareinsights.com/beacon.min.js"
-      data-cf-beacon='{"token": "598909c0c37347e1b62e72f772ba6199"}'
+      data-cf-beacon='{"token": "e3f23b72b4274ad9b97eb70a72e51c71"}'
       strategy="idle"
     />
   </StrictMode>
