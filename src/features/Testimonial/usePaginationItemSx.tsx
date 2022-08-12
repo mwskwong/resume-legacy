68cf8cb49301e2bfd8838b2315d5b8ceac35e476
Matthew Kwong <matthewkwong2@gmail.com>
2022-08-12 12:12:19 +0800

    feat: Increase border radius of `PaginationItem`

--- src/features/Testimonial/usePaginationItemSx.tsx
+++ src/features/Testimonial/usePaginationItemSx.tsx
@@ -6,14 +6,14 @@ const useSx: UsePaginationItemSx = active => ({
   root: {
     p: "4px",
     minWidth: "unset",
-    borderRadius: "5px"
+    borderRadius: "6px"
   },
   item: {
     width: active ? 20 : 8,
     height: 8,
     bgcolor: active ? "primary.main" : "currentColor",
     opacity: active ? 1 : .26,
-    borderRadius: "2.5px",
+    borderRadius: "3px",
     transition: theme => theme.transitions.create(["width", "opacity", "background-color"], {
       duration: theme.transitions.duration.shorter
     })
